a09b75476c3c23af0c09dcb5c8c5b6f09a9e2a0b
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2022-12-14 19:42:52 +0000

    Reorganised code into individual tasks

--- process_etroc1_data_dir.py
+++ process_etroc1_data_dir.py
@@ -18,7 +18,6 @@ def script_main(
         output_directory:Path,
         keep_only_triggers:bool,
         ):
-
     script_logger = logging.getLogger('process_dir')
 
     if not input_directory.is_dir():
@@ -33,79 +32,90 @@ def script_main(
     with RM.RunManager(out_dir) as Guilherme:
         Guilherme.create_run(raise_error=False)
 
-        with Guilherme.handle_task("proccess_etroc_data_directory", drop_old_data=True) as Bento:
-            with sqlite3.connect(Bento.task_path/'data.sqlite') as sqlite3_connection:
-                combined_df = pandas.DataFrame()
+        run_files = [x for x in input_directory.iterdir() if x.is_file() and str(x)[-11:-4] == "Split_0"]
 
-                files = [x for x in input_directory.iterdir() if x.is_file() and str(x)[-11:-4] == "Split_0"]
-                for file in files:
-                    path = Path(file)
-                    file_directory = str(path.name)[:-12]
-                    file_directory = Guilherme.path_directory/"Individual_Runs"/file_directory
-                    process_single_run(path, file_directory, keep_only_triggers, add_extra_data=False, drop_old_data=False)
+        with Guilherme.handle_task("process_etroc_data_directory", drop_old_data=True) as Turing:
+            for file in run_files:
+                path = Path(file)
+                file_base_name = str(path.name)[:-12]
+                file_directory = Guilherme.path_directory/"Individual_Runs"/file_base_name
 
-                    with RM.RunManager(file_directory) as Goku:
-                        with (Goku.path_directory/"cuts.csv").open("w") as cuts_file:
-                            cuts_file.write("calibration_code,<,200")
+                # Convert from RAW data format to our format
+                process_single_run(path, file_directory, keep_only_triggers, add_extra_data=False, drop_old_data=False)
 
-                    cut_single_run(file_directory, drop_old_data=False)
+                # Build a basic cuts.csv file
+                with RM.RunManager(file_directory) as Goku:
+                    with (Goku.path_directory/"cuts.csv").open("w") as cuts_file:
+                        cuts_file.write("calibration_code,<,200")
 
-                    with RM.RunManager(file_directory) as Goku:
-                        info = str(Goku.path_directory.name).split('_')
-                        if Goku.task_completed("apply_cuts"):
-                            sqlite_file = Goku.get_task_path("apply_cuts")/"data.sqlite"
-                        elif Goku.task_ran_successfully("proccess_etroc1_data_run"):
-                            sqlite_file = Goku.path_directory/"data"/"data.sqlite"
-                        else:
-                            sqlite_file = None
-                            script_logger.error("There is no data to process for run {}".format(Goku.run_name))
+                # Apply cuts
+                cut_single_run(file_directory, drop_old_data=False)
 
-                        if sqlite_file is not None:
-                            with sqlite3.connect(sqlite_file) as sqlite3_connection_run:
-                                #cursorObj = sqlite3_connection_run.cursor()
-                                combined_df = pandas.read_sql('SELECT data_board_id, COUNT(*) AS hits FROM etroc1_data GROUP BY data_board_id', sqlite3_connection_run, index_col=None)
+        if Guilherme.task_completed("process_etroc_data_directory"):
+            with Guilherme.handle_task("merge_runs", drop_old_data=False) as Bento:
+                with sqlite3.connect(Bento.task_path/'data.sqlite') as sqlite3_connection:
+                    for file in run_files:
+                        path = Path(file)
+                        file_base_name = str(path.name)[:-12]
+                        file_directory = Guilherme.path_directory/"Individual_Runs"/file_base_name
 
-                                combined_df["phase_adjust"] = info[2][8:]
-                                combined_df["pixel_id"] = None
-                                combined_df["board_injected_charge"] = None
-                                combined_df["board_discriminator_threshold"] = None
+                        with RM.RunManager(file_directory) as Goku:
+                            if Goku.task_completed("apply_cuts"):
+                                sqlite_file = Goku.get_task_path("apply_cuts")/"data.sqlite"
+                            elif Goku.task_ran_successfully("proccess_etroc1_data_run"):
+                                sqlite_file = Goku.path_directory/"data"/"data.sqlite"
+                            else:
+                                sqlite_file = None
+                                script_logger.error("There is no data to process for run {}".format(Goku.run_name))
 
-                                for idx in range(len(combined_df["data_board_id"])):
-                                    board_id = combined_df["data_board_id"][idx]
-                                    if board_id is None:
-                                        continue
+                            if sqlite_file is not None:
+                                info = str(Goku.path_directory.name).split('_')  # For retrieving metadata about the run later
 
-                                    #pixel_str = "pixel{}_id".format(board_id)
-                                    #inj_charge_str = "board{}_injected_charge".format(board_id)
-                                    #discr_str = "board{}_discriminator_threshold".format(board_id)
+                                with sqlite3.connect(sqlite_file) as sqlite3_connection_run:
+                                    combined_df = pandas.read_sql('SELECT data_board_id, COUNT(*) AS hits FROM etroc1_data GROUP BY data_board_id', sqlite3_connection_run, index_col=None)
+                                    # TODO: add a flag to count hits only on one board
 
-                                    if board_id == 3:
-                                        board_id = 2
-                                    combined_df["pixel_id"][idx] = info[3 + board_id*3]
-                                    combined_df["board_injected_charge"][idx] = info[4 + board_id*3][4:]
-                                    combined_df["board_discriminator_threshold"][idx] = info[5 + board_id*3][3:]
+                                    combined_df["phase_adjust"] = info[2][8:]
+                                    combined_df["pixel_id"] = None
+                                    combined_df["board_injected_charge"] = None
+                                    combined_df["board_discriminator_threshold"] = None
 
-                                combined_df = combined_df.dropna()
+                                    for idx in range(len(combined_df["data_board_id"])):
+                                        board_id = combined_df["data_board_id"][idx]
+                                        if board_id is None:
+                                            continue
 
-                                #combined_df["pixel0_id"] = info[3]
-                                #combined_df["board0_injected_charge"] = info[4][4:]
-                                #combined_df["board0_discriminator_threshold"] = info[5][3:]
-                                #combined_df["pixel1_id"] = info[6]
-                                #combined_df["board1_injected_charge"] = info[7][4:]
-                                #combined_df["board1_discriminator_threshold"] = info[8][3:]
-                                #combined_df["pixel3_id"] = info[9]
-                                #combined_df["board3_injected_charge"] = info[10][4:]
-                                #combined_df["board3_discriminator_threshold"] = info[11][3:]
+                                        #pixel_str = "pixel{}_id".format(board_id)
+                                        #inj_charge_str = "board{}_injected_charge".format(board_id)
+                                        #discr_str = "board{}_discriminator_threshold".format(board_id)
 
-                                script_logger.info('Saving run metadata into database...')
-                                combined_df.to_sql('combined_etroc1_data',
-                                          sqlite3_connection,
-                                          #index=False,
-                                          if_exists='append')
+                                        if board_id == 3:
+                                            board_id = 2
+                                        combined_df["pixel_id"][idx] = info[3 + board_id*3]
+                                        combined_df["board_injected_charge"][idx] = info[4 + board_id*3][4:]
+                                        combined_df["board_discriminator_threshold"][idx] = info[5 + board_id*3][3:]
 
-        if Guilherme.task_completed("proccess_etroc_data_directory"):
-            with Guilherme.handle_task("plot_combined", drop_old_data=True) as VanGogh:
-                sqlite_file = Guilherme.get_task_path("proccess_etroc_data_directory")/'data.sqlite'
+                                    combined_df = combined_df.dropna()
+
+                                    #combined_df["pixel0_id"] = info[3]
+                                    #combined_df["board0_injected_charge"] = info[4][4:]
+                                    #combined_df["board0_discriminator_threshold"] = info[5][3:]
+                                    #combined_df["pixel1_id"] = info[6]
+                                    #combined_df["board1_injected_charge"] = info[7][4:]
+                                    #combined_df["board1_discriminator_threshold"] = info[8][3:]
+                                    #combined_df["pixel3_id"] = info[9]
+                                    #combined_df["board3_injected_charge"] = info[10][4:]
+                                    #combined_df["board3_discriminator_threshold"] = info[11][3:]
+
+                                    script_logger.info('Saving run {} summary data into database...'.format(file_base_name))
+                                    combined_df.to_sql('combined_etroc1_data',
+                                              sqlite3_connection,
+                                              #index=False,
+                                              if_exists='append')
+
+        if Guilherme.task_completed("merge_runs"):
+            with Guilherme.handle_task("plot_combined", drop_old_data=False) as VanGogh:
+                sqlite_file = VanGogh.get_task_path("merge_runs")/'data.sqlite'
                 with sqlite3.connect(sqlite_file) as sqlite3_connection:
                     combined_df = pandas.read_sql('SELECT * FROM combined_etroc1_data', sqlite3_connection, index_col=None)
                     combined_df = combined_df.sort_values(by=['board_discriminator_threshold', 'data_board_id'])
