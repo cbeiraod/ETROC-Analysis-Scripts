ad7db70a02bc2c359d87d3912bcd87b0958c957d
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2022-12-29 21:50:44 +0000

    Fixed indentation

--- plot_etroc1_single_run.py
+++ plot_etroc1_single_run.py
@@ -16,126 +16,126 @@ def make_plots(
     base_path: Path,
     full_html: bool = False,  # For saving a html containing only a div with the plot
 ):
-            fig = px.histogram(
-                df,
-                x = 'calibration_code',
-                labels = {
-                    "calibration_code": "Calibration Code",
-                    "count": "Counts",
-                },
-                color='data_board_id',
-                title = "Histogram of Calibration Code<br><sup>Run: {}</sup>".format(run_name),
-            )
+    fig = px.histogram(
+        df,
+        x = 'calibration_code',
+        labels = {
+            "calibration_code": "Calibration Code",
+            "count": "Counts",
+        },
+        color='data_board_id',
+        title = "Histogram of Calibration Code<br><sup>Run: {}</sup>".format(run_name),
+    )
 
-            fig.write_html(
-                str(base_path/'calibration_code_histogram.html'),
-                full_html = full_html,
-                include_plotlyjs = 'cdn',
-            )
+    fig.write_html(
+        str(base_path/'calibration_code_histogram.html'),
+        full_html = full_html,
+        include_plotlyjs = 'cdn',
+    )
 
-            fig = px.histogram(
-                df,
-                x = 'time_of_arrival',
-                labels = {
-                    "time_of_arrival": "Time of Arrival",
-                    "count": "Counts",
-                },
-                color='data_board_id',
-                title = "Histogram of Time of Arrival<br><sup>Run: {}</sup>".format(run_name),
-            )
+    fig = px.histogram(
+        df,
+        x = 'time_of_arrival',
+        labels = {
+            "time_of_arrival": "Time of Arrival",
+            "count": "Counts",
+        },
+        color='data_board_id',
+        title = "Histogram of Time of Arrival<br><sup>Run: {}</sup>".format(run_name),
+    )
 
-            fig.write_html(
-                str(base_path/'time_of_arrival_histogram.html'),
-                full_html = full_html,
-                include_plotlyjs = 'cdn',
-            )
+    fig.write_html(
+        str(base_path/'time_of_arrival_histogram.html'),
+        full_html = full_html,
+        include_plotlyjs = 'cdn',
+    )
 
-            fig = px.histogram(
-                df,
-                x = 'time_over_threshold',
-                labels = {
-                    "time_over_threshold": "Time over Threshold",
-                    "count": "Counts",
-                },
-                color='data_board_id',
-                title = "Histogram of Time over Threshold<br><sup>Run: {}</sup>".format(run_name),
-            )
+    fig = px.histogram(
+        df,
+        x = 'time_over_threshold',
+        labels = {
+            "time_over_threshold": "Time over Threshold",
+            "count": "Counts",
+        },
+        color='data_board_id',
+        title = "Histogram of Time over Threshold<br><sup>Run: {}</sup>".format(run_name),
+    )
 
-            fig.write_html(
-                str(base_path/'time_over_threshold_histogram.html'),
-                full_html = full_html,
-                include_plotlyjs = 'cdn',
-            )
+    fig.write_html(
+        str(base_path/'time_over_threshold_histogram.html'),
+        full_html = full_html,
+        include_plotlyjs = 'cdn',
+    )
 
-            df["data_board_id_cat"] = df["data_board_id"].astype(str)
-            fig = px.scatter_matrix(
-                df,
-                dimensions=["time_of_arrival", "time_over_threshold", "calibration_code"],
-                labels = {
-                    "time_over_threshold": "Time over Threshold",
-                    "time_of_arrival": "Time of Arrival",
-                    "calibration_code": "Calibration Code",
-                    "data_board_id_cat": "Board ID",
-                },
-                color='data_board_id_cat',
-            )
-            fig.update_traces(
-                diagonal_visible=False,
-                showupperhalf=False
-            )
+    df["data_board_id_cat"] = df["data_board_id"].astype(str)
+    fig = px.scatter_matrix(
+        df,
+        dimensions=["time_of_arrival", "time_over_threshold", "calibration_code"],
+        labels = {
+            "time_over_threshold": "Time over Threshold",
+            "time_of_arrival": "Time of Arrival",
+            "calibration_code": "Calibration Code",
+            "data_board_id_cat": "Board ID",
+        },
+        color='data_board_id_cat',
+    )
 
-            fig.write_html(
-                base_path/'multi_scatter.html',
-                full_html = full_html,
-                include_plotlyjs = 'cdn',
-            )
+    fig.update_traces(
+        diagonal_visible=False,
+        showupperhalf=False
+    )
 
-            for board_id in df["data_board_id"].unique():
-                board_df = df.loc[df["data_board_id"] == board_id]
+    fig.write_html(
+        base_path/'multi_scatter.html',
+        full_html = full_html,
+        include_plotlyjs = 'cdn',
+    )
 
-                fig = px.density_heatmap(
-                    board_df,
-                    x="time_over_threshold",
-                    y="time_of_arrival",
-                    labels = {
-                        "time_over_threshold": "Time over Threshold",
-                        "time_of_arrival": "Time of Arrival",
-                        "data_board_id": "Board ID",
-                    },
-                    # marginal_x="histogram",
-                    # marginal_y="histogram",
-                    # color_continuous_scale="Viridis",  # https://plotly.com/python/builtin-colorscales/
-                    # facet_col='data_board_id',
-                    # facet_col_wrap=2,
-                )
+    for board_id in df["data_board_id"].unique():
+        board_df = df.loc[df["data_board_id"] == board_id]
 
-                fig.write_html(
-                    base_path/'Board{}_TOT_vs_TOA.html'.format(board_id),
-                    full_html = full_html,
-                    include_plotlyjs = 'cdn',
-                )
+        fig = px.density_heatmap(
+            board_df,
+            x="time_over_threshold",
+            y="time_of_arrival",
+            labels = {
+                "time_over_threshold": "Time over Threshold",
+                "time_of_arrival": "Time of Arrival",
+                "data_board_id": "Board ID",
+            },
+            # marginal_x="histogram",
+            # marginal_y="histogram",
+            # color_continuous_scale="Viridis",  # https://plotly.com/python/builtin-colorscales/
+            # facet_col='data_board_id',
+            # facet_col_wrap=2,
+        )
 
-            fig = px.density_heatmap(
-                df,
-                x="time_over_threshold",
-                y="time_of_arrival",
-                labels = {
-                    "time_over_threshold": "Time over Threshold",
-                    "time_of_arrival": "Time of Arrival",
-                    "data_board_id": "Board ID",
-                },
-                # marginal_x="histogram",
-                # marginal_y="histogram",
-                # color_continuous_scale="Viridis",  # https://plotly.com/python/builtin-colorscales/
-                facet_col='data_board_id',
-                facet_col_wrap=2,
-            )
+        fig.write_html(
+            base_path/'Board{}_TOT_vs_TOA.html'.format(board_id),
+            full_html = full_html,
+            include_plotlyjs = 'cdn',
+        )
 
-            fig.write_html(
-                base_path/'TOT_vs_TOA.html',
-                full_html = full_html,
-                include_plotlyjs = 'cdn',
-            )
+    fig = px.density_heatmap(
+        df,
+        x="time_over_threshold",
+        y="time_of_arrival",
+        labels = {
+            "time_over_threshold": "Time over Threshold",
+            "time_of_arrival": "Time of Arrival",
+            "data_board_id": "Board ID",
+        },
+        # marginal_x="histogram",
+        # marginal_y="histogram",
+        # color_continuous_scale="Viridis",  # https://plotly.com/python/builtin-colorscales/
+        facet_col='data_board_id',
+        facet_col_wrap=2,
+    )
+    fig.write_html(
+        base_path/'TOT_vs_TOA.html',
+        full_html = full_html,
+        include_plotlyjs = 'cdn',
+    )
 
 def plot_etroc1_task(
         Bob_Manager:RM.RunManager,
