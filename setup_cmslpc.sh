1c72f8291446258d4093009633cbf4d8fde2f54a
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2023-01-03 11:01:27 +0000

    Added setup file for CMSLPC

---
+++ setup_cmslpc.sh
@@ -0,0 +1,6 @@
+#!/bin/env bash
+
+#### Use python 3.8.6
+source /cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc8-opt/setup.sh
+
+export PYTHONPATH=~/.local/lib/python3.8/site-packages:$PYTHONPATH
