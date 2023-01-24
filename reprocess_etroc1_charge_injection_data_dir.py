373d797fdfba20ffd87fa09b43e3fabcfc3fcdba
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2023-01-24 13:33:47 +0000

    Modified reprocess so it doesn't erase the new cut options

--- reprocess_etroc1_charge_injection_data_dir.py
+++ reprocess_etroc1_charge_injection_data_dir.py
@@ -5,49 +5,55 @@ import lip_pps_run_manager as RM
 
 import logging
 from datetime import datetime
-from process_etroc1_charge_injection_data_dir import process_etroc1_data_directory_task
+from process_etroc1_charge_injection_data_dir import apply_cuts_to_single_runs_subtask
 from process_etroc1_charge_injection_data_dir import merge_etroc1_runs_task
 from process_etroc1_charge_injection_data_dir import plot_etroc1_combined_task
 
+def reprocess_etroc1_data_directory_task(
+    AdaLovelace: RM.RunManager,
+    script_logger: logging.Logger,
+    run_dirs: list[Path],
+    make_plots:bool = False,
+):
+    if AdaLovelace.task_completed("process_etroc1_data_directory"):
+        with AdaLovelace.handle_task("reprocess_etroc1_data_directory", drop_old_data=True) as Turing:
+            run_dirs = []
+
+            apply_cuts_to_single_runs_subtask(
+                Turing,
+                script_logger=script_logger,
+                run_dirs=run_dirs,
+                make_plots=make_plots,
+            )
 
 def script_main(
-        input_directory:Path,
         output_directory:Path,
-        keep_only_triggers:bool,
         make_plots:bool,
-        add_date:bool = True,
         board0_default:int = 535,
         board1_default:int = 720,
         board3_default:int = 720,
         filter_default:bool = True,
         trigger_board:int = 0,
         ):
-    script_logger = logging.getLogger('process_dir')
+    script_logger = logging.getLogger('reprocess_dir')
 
-    if not input_directory.is_dir():
-        script_logger.info("The input directory should be an existing directory")
+    if not output_directory.is_dir():
+        script_logger.info("The output directory should be an existing directory")
         return
 
-    if add_date:
-        now = datetime.now() # current date and time
-
-        out_dir = output_directory.resolve()
-        out_dir = out_dir.parent / (now.strftime("%Y%m%d-") + out_dir.name)
-    else:
-        out_dir = output_directory.resolve()
+    out_dir = output_directory.resolve()
 
     with RM.RunManager(out_dir) as Guilherme:
-        Guilherme.create_run(raise_error=True)
+        Guilherme.create_run(raise_error=False)
 
-        run_files = [x for x in input_directory.iterdir() if x.is_file() and str(x)[-11:-4] == "Split_0"]
+        #run_files = [x for x in input_directory.iterdir() if x.is_file() and str(x)[-11:-4] == "Split_0"]
+        run_dirs = [x for x in (out_dir/"Individual_Runs").iterdir() if x.is_dir()]
 
-        process_etroc1_data_directory_task(
+        reprocess_etroc1_data_directory_task(
             Guilherme,
             script_logger=script_logger,
-            run_files=run_files,
-            keep_only_triggers=keep_only_triggers,
+            run_dirs=run_dirs,
             make_plots=make_plots,
-            first_time=False,
         )
 
         merge_etroc1_runs_task(
@@ -70,12 +76,12 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Converts all individual data file from a directory of data taken with the KC 705 FPGA development board connected to an ETROC1 into our data format')
     parser.add_argument(
-        '-d',
-        '--directory',
+        '-o',
+        '--out-directory',
         metavar = 'path',
-        help = "Path to the directory with the measurements' data.",
-        required = True,
-        dest = 'directory',
+        help = 'Path to the output directory for the run data. Default: ./out',
+        default = "./out",
+        dest = 'out_directory',
         type = str,
     )
     parser.add_argument(
@@ -93,34 +99,11 @@ if __name__ == '__main__':
         dest = 'log_file',
     )
     parser.add_argument(
-        '-o',
-        '--out-directory',
-        metavar = 'path',
-        help = 'Path to the output directory for the run data. Default: ./out',
-        default = "./out",
-        dest = 'out_directory',
-        type = str,
-    )
-    parser.add_argument(
-        '-k',
-        '--keep-all',
-        help = "If set, all lines from the raw data file will be kept in the output, if not, only those corresponding to hits will be kept",
-        action = 'store_true',
-        dest = 'keep_all',
-    )
-    parser.add_argument(
         '-p',
         '--make_plots',
         help = "If set, the plots for the individual runs will be created",
         action = 'store_true',
         dest = 'make_plots',
-    )
-    parser.add_argument(
-        '-i',
-        '--inhibit_date',
-        help = "If set, the date will not be added to the start of the directory name",
-        action = 'store_true',
-        dest = 'inhibit_date',
     )
     parser.add_argument(
         '-b0',
@@ -185,11 +168,8 @@ if __name__ == '__main__':
             logging.basicConfig(level=0)
 
     script_main(
-        Path(args.directory),
         Path(args.out_directory),
-        keep_only_triggers = not args.keep_all,
         make_plots = args.make_plots,
-        add_date = not args.inhibit_date,
         board0_default = args.board0_default,
         board1_default = args.board1_default,
         board3_default = args.board3_default,
